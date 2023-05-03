e9b90599c5542b0b92b5de8c0137411c05145293
Freek <103433440+FreekStraten@users.noreply.github.com>
2023-05-03 14:26:05 +0200

    added api for packet create (use postman for sending)

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::post('/packets', [PacketController::class, 'store']);
+
+//create an API for the packets store
+Route::middleware('auth:sanctum')->post('/packetAPI', [App\Http\Controllers\PacketController::class, 'store']);
+
+
+
+
