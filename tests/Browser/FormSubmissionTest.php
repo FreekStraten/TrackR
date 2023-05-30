42024aafe50aa1270a4e8a2849c1ba252cf2933e
Freek <103433440+FreekStraten@users.noreply.github.com>
2023-05-30 11:52:32 +0200

    .

---
+++ tests/Browser/FormSubmissionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class FormSubmissionTest extends DuskTestCase
+{
+    //create a dusk test for the form submission packetCreate
+
+}
