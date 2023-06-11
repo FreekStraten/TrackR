3cc6374af965c2cfe88fab089ddac6d962c1cd2d
Cas Denteneer <c.denteneer@student.avans.nl>
2023-06-11 14:31:03 +0200

    Language test

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -65,7 +65,7 @@
                     <!-- dropdown to change language -->
                     <x-dropdown align="left" width="48">
                         <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150 mt-3">
+                            <button dusk="open-nav-lang" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150 mt-3">
                                 <div>{{ __('nav.language') }}</div>
                                 <div class="ml-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -75,10 +75,10 @@
                             </button>
                         </x-slot>
                         <x-slot name="content">
-                            <x-dropdown-link :href="route('locale', ['locale' => 'en'])">
+                            <x-dropdown-link dusk="lang-english" :href="route('locale', ['locale' => 'en'])">
                                 {{ __('nav.english') }}
                             </x-dropdown-link>
-                            <x-dropdown-link :href="route('locale', ['locale' => 'nl'])">
+                            <x-dropdown-link dusk="lang-dutch" :href="route('locale', ['locale' => 'nl'])">
                                 {{ __('nav.dutch') }}
                             </x-dropdown-link>
                         </x-slot>
---
+++ tests/Browser/ChangeLanguageTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ChangeLanguageTest extends DuskTestCase
+{
+    public function testLogin(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->pause(500)
+                ->type('email', 'normal@user.com')
+                ->pause(500)
+                ->type('password', 'user')
+                ->pause(500)
+                ->click('@login')
+                ->pause(750)
+                ->visit('/')
+                ->pause(750)
+                ->assertPathIs('/packets');
+        });
+    }
+
+    public function testSwitchLangToDutch(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertAuthenticated()
+                ->click('@open-nav-lang') // Click on the dropdown trigger button (update the selector if needed
+                ->pause(500)
+                ->click('@lang-dutch') // Click on the English language button (update the selector if needed)
+                ->pause(500)
+                ->assertSee('Taal');
+        });
+
+    }
+
+    public function testSwitchLangToEnglish(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertAuthenticated()
+                ->click('@open-nav-lang') // Click on the dropdown trigger button (update the selector if needed
+                ->pause(500)
+                ->click('@lang-english') // Click on the English language button (update the selector if needed)
+                ->pause(500)
+                ->assertSee('Language');
+        });
+
+    }
+
+
+}
